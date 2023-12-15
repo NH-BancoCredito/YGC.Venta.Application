012ff3e6d10564ceb1e093fc84e34bda0c5b30f8
JHON PIER <jhonpierta@gmail.com>
2023-12-14 22:05:07 -0500

    add handler

---
+++ Venta.Application/CasosUso/Venta/VentaHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venta.Application.CasosUso.Venta
+{
+    internal class VentaHandler
+    {
+    }
+}
